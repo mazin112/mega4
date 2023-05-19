cca2a7123f47458e7c13109c01f1ddd5579a3352
mazin112 <120086917+mazin112@users.noreply.github.com>
2023-05-19 14:34:40 +0200

    bot.py

---
+++ bot.py
@@ -0,0 +1,90 @@
+import os
+
+import telegram
+
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+
+# Create a Telegram bot using your API token
+
+bot = telegram.Bot(token='5723010945:AAFes5mjH6Q4zWNMIGYUFBlyIjU-qFXLHT8')
+
+# Handler for the /start command
+
+def start(update, context):
+
+    update.message.reply_text("Welcome to the Mega Bot! Send me any text or use the buttons below.")
+
+# Handler for processing text messages
+
+def process_text(update, context):
+
+    text = update.message.text
+
+    chat_id = update.message.chat_id
+
+    if text == "/space":
+
+        # Add code to calculate remaining space in the Mega account
+
+        remaining_space = calculate_remaining_space()
+
+        update.message.reply_text(f"Remaining space in Mega account: {remaining_space} MB")
+
+    elif text == "/delete":
+
+        # Add code to delete videos from the Mega account
+
+        delete_videos()
+
+        update.message.reply_text("Videos deleted from Mega account.")
+
+    else:
+
+        update.message.reply_text("Invalid command. Use the buttons below.")
+
+# Handler for processing button clicks
+
+def button_click(update, context):
+
+    query = update.callback_query
+
+    chat_id = query.message.chat_id
+
+    if query.data == "upload":
+
+        # Add code to handle file upload to Mega
+
+        # You can use third-party libraries like Mega.py or PyDrive for interacting with Mega
+
+        # Placeholder code for demonstration purposes
+
+        update.callback_query.answer("Uploading file to Mega...")
+
+        update.callback_query.edit_message_text("File uploaded to Mega.")
+
+    else:
+
+        update.callback_query.answer("Invalid button.")
+
+# Create an Updater and attach handlers
+
+updater = Updater(token='YOUR_TELEGRAM_BOT_TOKEN', use_context=True)
+
+dispatcher = updater.dispatcher
+
+# Command handlers
+
+dispatcher.add_handler(CommandHandler("start", start))
+
+# Text message handler
+
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, process_text))
+
+# Button click handler
+
+dispatcher.add_handler(CallbackQueryHandler(button_click))
+
+# Start the bot
+
+updater.start_polling()
+
